d684654d3a9548c12e3230ec67803fe7e4e1f5b2
Simon Schlegl <simonschlegl@dhcp-10-177-118-156.dynamic.eduroam.mwn.de>
2015-12-12 22:17:35 +0100

    added dummy main menu and level1

---
+++ OmNomLight/Assets/Scripts/GameState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameState : MonoBehaviour {
+
+	private Collectible[] m_collectables;
+
+	private int totalCollectibles;
+	private int eatenCollectibles = 0;
+	private int refridgeratedCollectibleds = 0;
+
+	private bool isOver = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		totalCollectibles = GameObject.FindObjectsOfType<Collectible>().Length;
+	}
+
+	bool IsOver()
+	{
+		return isOver;
+	}
+
+	void CheckGameState()
+	{
+		//if( )
+	}
+
+}
---
+++ OmNomLight/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if( Input.GetButtonDown("Pick Up") )
+		{
+			Application.LoadLevel("Level1");
+		}
+	}
+}
