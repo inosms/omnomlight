0f545022081275b25c957d4e8597a78da0471097
Robert <robert.brand@tum.de>
2015-12-12 15:58:26 +0100

    Added functionality for picking up objects

---
+++ OmNomLight/Assets/Scripts/Objects/GetCarried.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetCarried : MonoBehaviour 
+{
+    public bool beingCarried = false;
+
+    public void startCarrying(Transform carry)
+    {
+        transform.SetParent(carry, true);
+        beingCarried = true;
+    }
+
+    public void stopCarrying()
+    {
+        transform.SetParent(null, true);
+        beingCarried = false;
+    }
+}
---
+++ OmNomLight/Assets/Scripts/PickUp.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour
+{
+    public float range = 0.5f;
+
+    bool carrying = false;
+    private GetCarried carriedObject;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Pick Up"))
+        {
+            if(!carrying)//currently not carrying anything
+            {
+                RaycastHit2D hit = Physics2D.CircleCast(transform.position, range, Vector3.back);
+
+                //check the pickup zone
+                if (hit)
+                {
+                    carriedObject = hit.collider.GetComponent<GetCarried>();
+
+                    //an object that can be carried is inside the pickup zone
+                    if (carriedObject)
+                    {
+                        carriedObject.startCarrying(transform);
+                        carrying = true;
+                    }
+                }
+            }
+            else if (carriedObject)
+            {
+                //drop the carried object
+                carriedObject.stopCarrying();
+                carrying = false;
+            }
+        }
+    }
+
+}
