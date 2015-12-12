dabce6156fc7876813118b3cf6adb6488bbc4711
Robert <robert.brand@tum.de>
2015-12-12 04:28:54 +0100

    Added ImageEffect

---
+++ OmNomLight/Assets/Scripts/Effects/AlphaMap.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class AlphaMap : MonoBehaviour
+{
+    public Material mat;
+
+    //Basically only applies image effect
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
