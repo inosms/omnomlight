436b92df0b7e0d945e39c6ccdee33be7849e285c
Simon Schlegl <simonschlegl@dhcp-10-177-118-156.dynamic.eduroam.mwn.de>
2015-12-13 12:05:38 +0100

    added action key for monster

--- OmNomLight/Assets/Scripts/Collectible.cs
+++ OmNomLight/Assets/Scripts/Collectible.cs
@@ -10,33 +10,12 @@ public class Collectible : MonoBehaviour
         {
             OnPickup();
 
-			// Play on random music clip
-			int tmp_random = Random.Range(0,4);
-			if( tmp_random == 0 )
-				c.GetComponent<AudioSource>().clip = c.GetComponent<PlayerController>().audioEating1;
-			else if( tmp_random == 1)
-				c.GetComponent<AudioSource>().clip = c.GetComponent<PlayerController>().audioEating2;
-			else if( tmp_random == 2 )
-				c.GetComponent<AudioSource>().clip = c.GetComponent<PlayerController>().audioEating3;
-			else
-				c.GetComponent<AudioSource>().clip = c.GetComponent<PlayerController>().audioEating4;
-			c.GetComponent<AudioSource>().Play();
+
         }
     }
 
     void OnPickup()
     {
-		GameObject.FindObjectOfType<GameState>().MonsterEatsOneFood();
-
-		// When Human holds the object, let the human first drop the object
-		if( GameObject.FindObjectOfType<PickUp>().GetCarriedObject() == this.GetComponent<GetCarried>() )
-		{
-			GameObject.FindObjectOfType<PickUp>().StopCarrying();
-		}
-
-		// disable the object to prevent further collisions
-		this.gameObject.SetActive(false);
-
-        Destroy(this.gameObject, 0.3f);
+		
     }
 }
--- OmNomLight/Assets/Scripts/PickUp.cs
+++ OmNomLight/Assets/Scripts/PickUp.cs
@@ -13,9 +13,8 @@ public class PickUp : MonoBehaviour
     void Update()
     {
 		
-        if(Input.GetButtonDown("Pick Up"))
+		if(Input.GetButtonDown("Pick Up") || Input.GetButtonDown("Pick Up Controller"))
         {
-			
             if(!carrying)//currently not carrying anything
             {
 				RaycastHit2D hit = Physics2D.CircleCast(transform.position, range, Vector3.back,range,layerMask);
@@ -30,11 +29,10 @@ public class PickUp : MonoBehaviour
                     //an object that can be carried is inside the pickup zone
                     if (carriedObject)
                     {
-						GetComponent<AudioSource>().clip = GetComponent<PlayerController>().audioPickUp;
-						GetComponent<AudioSource>().Play();
-
-                        carriedObject.startCarrying(transform);
-                        carrying = true;
+						if( gameObject.CompareTag("Monster") && Input.GetButtonDown("Pick Up Controller"))
+							MonsterAction(carriedObject);
+						else if(gameObject.CompareTag("Human") && Input.GetButtonDown("Pick Up") )
+							HumanAction(carriedObject);
                     }
                 }
             }
@@ -49,6 +47,49 @@ public class PickUp : MonoBehaviour
             }
         }
     }
+
+	private void MonsterAction(GetCarried n_object)
+	{
+		Debug.Log("Monster Action");
+		Collectible tmp_food = n_object.GetComponent<Collectible>();
+
+		// only if the object really is food; prevent eating the Candle
+		if( n_object.GetComponent<Collectible>() != null )
+		{
+			// Play on random music clip
+			int tmp_random = Random.Range(0,4);
+			if( tmp_random == 0 )
+				gameObject.GetComponent<AudioSource>().clip = gameObject.GetComponent<PlayerController>().audioEating1;
+			else if( tmp_random == 1)
+				gameObject.GetComponent<AudioSource>().clip = gameObject.GetComponent<PlayerController>().audioEating2;
+			else if( tmp_random == 2 )
+				gameObject.GetComponent<AudioSource>().clip = gameObject.GetComponent<PlayerController>().audioEating3;
+			else
+				gameObject.GetComponent<AudioSource>().clip = gameObject.GetComponent<PlayerController>().audioEating4;
+			gameObject.GetComponent<AudioSource>().Play();
+
+			// When Human holds the object, let the monster not eat stuff
+			if( GameObject.FindObjectOfType<PickUp>().GetCarriedObject() != this.GetComponent<GetCarried>() )
+			{
+				GameObject.FindObjectOfType<GameState>().MonsterEatsOneFood();
+
+				GameObject.FindObjectOfType<PickUp>().StopCarrying();
+
+				n_object.gameObject.SetActive(false);
+
+				Destroy(n_object.gameObject, 0.3f);
+			}
+		}
+	}
+
+	private void HumanAction(GetCarried n_object)
+	{
+		GetComponent<AudioSource>().clip = GetComponent<PlayerController>().audioPickUp;
+		GetComponent<AudioSource>().Play();
+
+		carriedObject.startCarrying(transform);
+		carrying = true;
+	}
 
 	public void StopCarrying()
 	{
