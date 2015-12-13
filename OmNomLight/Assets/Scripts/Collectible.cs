428bb11351913eceafa7fc78f68cf606d9c3dc43
Simon Schlegl <simonschlegl@dhcp-10-177-118-156.dynamic.eduroam.mwn.de>
2015-12-13 11:44:20 +0100

    fixed bug when monster eats stuff the human holds

--- OmNomLight/Assets/Scripts/Collectible.cs
+++ OmNomLight/Assets/Scripts/Collectible.cs
@@ -27,7 +27,16 @@ public class Collectible : MonoBehaviour
     void OnPickup()
     {
 		GameObject.FindObjectOfType<GameState>().MonsterEatsOneFood();
+
+		// When Human holds the object, let the human first drop the object
+		if( GameObject.FindObjectOfType<PickUp>().GetCarriedObject() == this.GetComponent<GetCarried>() )
+		{
+			GameObject.FindObjectOfType<PickUp>().StopCarrying();
+		}
+
+		// disable the object to prevent further collisions
 		this.gameObject.SetActive(false);
+
         Destroy(this.gameObject, 0.3f);
     }
 }
